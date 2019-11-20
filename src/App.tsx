66184104cdc283fd70d8e4062f0a1bf732c76982
nkgrnkgr <beefupme@gmail.com>
2019-11-20 21:30:45 +0900

    未ログイン時に何も表示されなくなる問題を解決

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
+import { createStyles, makeStyles } from '@material-ui/styles';
 import { Route } from 'react-router-dom';
-import { DashboardPage } from './pages/dashboard/components/DashboardPage';
-import { LandingPage } from './pages/landing/components/LandingPage';
-import { makeStyles, createStyles } from '@material-ui/styles';
-import { Theme } from '@material-ui/core';
-import { SettingPage } from './pages/setting/components/SettingPage';
-import { SignInPage } from './pages/signin/components/SignInPage';
-import { FirebaseAuthInitializer } from './pages/shared/components/FirebaseAuthInitializer';
 import { ApplicationContextProvider } from './contexts/ApplicationContext';
-import { UserContextProvider } from './contexts/UserContext';
-import { Eventpage } from './pages/events/containers/EventPage';
-import { Auth } from './pages/shared/components/Auth';
-import { ScrollTop } from './pages/shared/components/ScrollTop';
 import { EventPageContextProvider } from './contexts/EventPageContext';
+import { UserContextProvider } from './contexts/UserContext';
 import { ApiCallbackPage } from './pages/apicallbak/containers/ApiCallbackPage';
+import { DashboardPage } from './pages/dashboard/components/DashboardPage';
+import { Eventpage } from './pages/events/containers/EventPage';
+import { LandingPage } from './pages/landing/components/LandingPage';
+import { SettingPage } from './pages/setting/components/SettingPage';
+import { Auth } from './pages/shared/components/Auth';
+import { FirebaseAuthInitializer } from './pages/shared/components/FirebaseAuthInitializer';
+import { ScrollTop } from './pages/shared/components/ScrollTop';
 import { UserUpdater } from './pages/shared/containers/UserUpdater';
+import { SignInPage } from './pages/signin/components/SignInPage';
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(
   createStyles({
     root: {
       flexGrow: 1,
@@ -32,19 +31,19 @@ const App: React.FC = () => {
       <UserContextProvider>
         <EventPageContextProvider>
           <FirebaseAuthInitializer>
-            <UserUpdater>
-              <div className={classes.root}>
-                <ScrollTop />
-                <Route exact path="/" component={LandingPage} />
-                <Route exact path="/signin" component={SignInPage} />
-                <Route path="/apicallback" component={ApiCallbackPage} />
-                <Auth>
+            <div className={classes.root}>
+              <ScrollTop />
+              <Route exact path="/" component={LandingPage} />
+              <Route exact path="/signin" component={SignInPage} />
+              <Route path="/apicallback" component={ApiCallbackPage} />
+              <Auth>
+                <UserUpdater>
                   <Route path="/dashboard" component={DashboardPage} />
                   <Route path="/setting" component={SettingPage} />
                   <Route path="/events/:eventId" component={Eventpage} />
-                </Auth>
-              </div>
-            </UserUpdater>
+                </UserUpdater>
+              </Auth>
+            </div>
           </FirebaseAuthInitializer>
         </EventPageContextProvider>
       </UserContextProvider>
--- src/pages/shared/containers/UserUpdater.tsx
+++ src/pages/shared/containers/UserUpdater.tsx
@@ -29,7 +29,7 @@ export const UserUpdater: React.FC = ({ children }) => {
     return <></>;
   }
 
-  if (error || !user) {
+  if (error) {
     return <></>;
   }
 
