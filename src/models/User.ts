e0e969f3d3b841ebd82dbc541f2360c338b13bc1
nkgrnkgr <beefupme@gmail.com>
2019-11-21 13:03:52 +0900

    WIP anonymous

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,11 +1,5 @@
-import black from '../images/anonymous-black.png';
-import blue from '../images/anonymous-blue.png';
-import green from '../images/anonymous-green.png';
-import orange from '../images/anonymous-orange.png';
-import pink from '../images/anonymous-pink.png';
-import purple from '../images/anonymous-purple.png';
-import red from '../images/anonymous-red.png';
 import { useState } from 'react';
+import { AnonymousColor } from './Anonymous';
 
 export interface UserModel {
   displayName: string;
@@ -13,8 +7,31 @@ export interface UserModel {
   uid: string;
   eventIdsParticipated: string[];
   isAnonymous?: boolean;
-  anonymousColor?: AnonymousColor;
   twitterIntegration?: TwitterIntegration;
+}
+
+export class User implements UserModel {
+  displayName = '';
+  avatarUrl = '';
+  uid: string;
+  eventIdsParticipated: string[];
+  isAnonymous?: boolean | undefined;
+  twitterIntegration?: TwitterIntegration | undefined;
+
+  constructor(user: firebase.User) {
+    const { isAnonymous, uid, providerData } = user;
+    if (
+      providerData[0] &&
+      providerData[0].displayName &&
+      providerData[0].photoURL
+    ) {
+      this.displayName = providerData[0].displayName;
+      this.avatarUrl = providerData[0].photoURL;
+    }
+    this.uid = uid;
+    this.isAnonymous = isAnonymous;
+    this.eventIdsParticipated = [];
+  }
 }
 
 export const initialUserData: UserModel = {
@@ -30,53 +47,6 @@ export interface TwitterIntegration {
   accessTokenSecret: string;
 }
 
-export interface AnonymousColor {
-  index: number;
-  name: string;
-  hex: string;
-  anonymousImage: string;
-}
-
-export const ANONYMOUS_COLOR_HEX = [
-  '#000000',
-  '#3f92e3',
-  '#548e6a',
-  '#f0b43a',
-  '#f6cce5',
-  '#51447c',
-  '#e84259',
-];
-
-export const ANONYMOUS_COLOR_NAME = [
-  'black',
-  'blue',
-  'green',
-  'orange',
-  'pink',
-  'purple',
-  'red',
-];
-
-export const ANONYMOUS_COLOR_IMAGE = [
-  black,
-  blue,
-  green,
-  orange,
-  pink,
-  purple,
-  red,
-];
-
-const createColors = (): AnonymousColor[] =>
-  ANONYMOUS_COLOR_NAME.map((name, index) => ({
-    index,
-    name,
-    hex: ANONYMOUS_COLOR_HEX[index],
-    anonymousImage: ANONYMOUS_COLOR_IMAGE[index],
-  }));
-
-export const ANONYMOUS_COLORS = createColors();
-
 export const getAvatarUrl = (user: UserModel) => {
   if (user.isAnonymous || !user.avatarUrl) {
     return user.anonymousColor
@@ -85,19 +55,4 @@ export const getAvatarUrl = (user: UserModel) => {
   }
 
   return user.avatarUrl;
-};
-
-export const useUserColor = (
-  anonymousColor: AnonymousColor = ANONYMOUS_COLORS[0],
-): [AnonymousColor, (hex: string) => void] => {
-  const [color, setColor] = useState(anonymousColor);
-
-  const selectColorWithHex = (hex: string) => {
-    const index = ANONYMOUS_COLOR_HEX.indexOf(hex);
-    if (index > -1) {
-      setColor(ANONYMOUS_COLORS[index]);
-    }
-  };
-
-  return [color, selectColorWithHex];
 };
