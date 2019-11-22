972245c4aec54dd046f8658f11d360542fd27a65
nkgrnkgr <beefupme@gmail.com>
2019-11-22 12:57:35 +0900

    参加したイベントの件数が0件の場合の表示を修正

---
+++ src/pages/dashboard/components/EmptyEventList.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import empty from '../../../images/_empty.svg';
+import { Typography, Theme } from '@material-ui/core';
+import { makeStyles, createStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+    },
+    image: {
+      margin: theme.spacing(5),
+      maxWidth: '50%',
+    },
+  }),
+);
+
+export const EmptyEventList: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <img src={empty} className={classes.image} />
+      <Typography variant="body1">
+        今まで参加したイベントはありません
+      </Typography>
+    </div>
+  );
+};
--- src/pages/dashboard/components/ParticipatedEventList.tsx
+++ src/pages/dashboard/components/ParticipatedEventList.tsx
@@ -12,6 +12,7 @@ import { IconLink } from '../../shared/components/IconLink';
 import { EventModel } from '../../../models/Event';
 import { getYearMonthDayHourMitutes } from '../../../utils/datetime';
 import { useHistory } from 'react-router';
+import { EmptyEventList } from './EmptyEventList';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -32,25 +33,31 @@ export const ParticipatedEventList: React.FC<Props> = ({ eventList }) => {
 
   return (
     <div>
-      <div className={classes.participatedEventList}>
-        <List component="nav" aria-label="visited event list">
-          {eventList.map(event => (
-            <ListItem
-              button
-              key={event.name}
-              onClick={() => history.push(`/events/${event.id}`)}
-            >
-              <ListItemIcon>
-                <IconLink title={event.name} className="far fa-calendar-alt" />
-              </ListItemIcon>
-              <ListItemText
-                primary={event.name}
-                secondary={getYearMonthDayHourMitutes(event.date)}
-              />
-            </ListItem>
-          ))}
-        </List>
-      </div>
+      {eventList.length === 0 && <EmptyEventList />}
+      {eventList.length !== 0 && (
+        <div className={classes.participatedEventList}>
+          <List component="nav" aria-label="visited event list">
+            {eventList.map(event => (
+              <ListItem
+                button
+                key={event.name}
+                onClick={() => history.push(`/events/${event.id}`)}
+              >
+                <ListItemIcon>
+                  <IconLink
+                    title={event.name}
+                    className="far fa-calendar-alt"
+                  />
+                </ListItemIcon>
+                <ListItemText
+                  primary={event.name}
+                  secondary={getYearMonthDayHourMitutes(event.date)}
+                />
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      )}
     </div>
   );
 };
