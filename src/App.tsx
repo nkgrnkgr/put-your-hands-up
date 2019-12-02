0267322227883817c017d408fe63fb35e621a137
nkgrnkgr <beefupme@gmail.com>
2019-12-02 17:40:49 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -42,6 +42,7 @@ const App: React.FC = () => {
                 <FirebaseAuthLoadedListener>
                   <div className={classes.root}>
                     <ScrollTop />
+
                     <Notification />
                     <Switch>
                       <Route exact path="/" component={LandingPage} />
