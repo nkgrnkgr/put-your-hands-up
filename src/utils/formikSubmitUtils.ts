73fbce594f6a960fcc993a0b72ab34304b6cdd7e
nkgrnkgr <beefupme@gmail.com>
2019-12-14 21:17:42 +0900

    update for time bug

--- src/utils/formikSubmitUtils.ts
+++ src/utils/formikSubmitUtils.ts
@@ -1,14 +1,10 @@
 import { FormikHelpers } from 'formik';
-import { now } from './datetime';
 
 export const onFormikSubmitHandler = <T, R>(
-  values: T,
   submitValues: R,
   action: FormikHelpers<T>,
-  updateTimeField: keyof T & string,
 ) =>
   new Promise<R>(resolve => {
-    action.setFieldValue(updateTimeField, now());
     action.setSubmitting(false);
     resolve(submitValues);
     action.resetForm();
