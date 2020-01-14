6b104c2ba6190c5e56dbabec60626618744d15b8
nkgrnkgr <beefupme@gmail.com>
2020-01-14 23:27:27 +0900

    WIP add Plus Thread5

--- src/pages/events/components/ModalNoteForm.tsx
+++ src/pages/events/components/ModalNoteForm.tsx
@@ -1,17 +1,9 @@
-import {
-  createStyles,
-  Divider,
-  makeStyles,
-  Modal,
-  Theme,
-  Paper,
-  Backdrop,
-  Fade,
-} from '@material-ui/core';
+import { Divider } from '@material-ui/core';
 import { FormikProps } from 'formik';
 import React from 'react';
 import { NoteContentsModel } from '../../../models/Note';
 import { UserModel } from '../../../models/User';
+import { ModalBase } from '../../shared/components/ModalBase';
 import { ColorSelection } from './ColorSelection';
 import { CommentForm } from './CommentForm';
 import { NoteButtons } from './NoteButtons';
@@ -29,21 +21,6 @@ interface OuterProps {
 
 type Props = OuterProps & FormikProps<NoteContentsModel>;
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    paper: {
-      maxWidth: 800,
-      padding: theme.spacing(1, 3),
-      margin: theme.spacing(1),
-    },
-  }),
-);
-
 export const ModalNoteForm: React.FC<Props> = props => {
   const {
     handleSubmit,
@@ -56,42 +33,27 @@ export const ModalNoteForm: React.FC<Props> = props => {
     toggleTwitterShare,
   } = props;
 
-  const classes = useStyles();
-
   return (
-    <Modal
-      open={open}
-      className={classes.root}
-      closeAfterTransition
-      BackdropComponent={Backdrop}
-      BackdropProps={{
-        timeout: 500,
-      }}
-      onClose={() => onClose()}
-    >
-      <Fade in={open}>
-        <Paper className={classes.paper}>
-          <form onSubmit={handleSubmit}>
-            <CommentForm rowCount={'4'} {...props} handleOnForcus={() => {}} />
-            <RecommendComments
-              handleOnClick={(comment: string) =>
-                setFieldValue('comment', values.comment + comment)
-              }
-            />
-            <Divider />
-            <TagForm {...props} />
-            <Divider />
-            <ColorSelection {...props} />
-            <Divider />
-            <NoteButtons
-              handleOnClickCloseButton={() => onClose()}
-              sholdShowTwitter={sholdShowTwitter}
-              sholdTwitterShare={sholdTwitterShare}
-              toggleTwitterShare={toggleTwitterShare}
-            />
-          </form>
-        </Paper>
-      </Fade>
-    </Modal>
+    <ModalBase open={open} onClose={onClose}>
+      <form onSubmit={handleSubmit}>
+        <CommentForm rowCount={'4'} {...props} />
+        <RecommendComments
+          handleOnClick={(comment: string) =>
+            setFieldValue('comment', values.comment + comment)
+          }
+        />
+        <Divider />
+        <TagForm {...props} />
+        <Divider />
+        <ColorSelection {...props} />
+        <Divider />
+        <NoteButtons
+          handleOnClickCloseButton={() => onClose()}
+          sholdShowTwitter={sholdShowTwitter}
+          sholdTwitterShare={sholdTwitterShare}
+          toggleTwitterShare={toggleTwitterShare}
+        />
+      </form>
+    </ModalBase>
   );
 };
--- src/pages/shared/components/ConfirmDialog.tsx
+++ src/pages/shared/components/ConfirmDialog.tsx
@@ -1,15 +1,12 @@
-import React from 'react';
 import {
-  Backdrop,
+  Button,
   createStyles,
   makeStyles,
-  Modal,
   Theme,
-  Fade,
-  Paper,
   Typography,
-  Button,
 } from '@material-ui/core';
+import React from 'react';
+import { ModalBase } from './ModalBase';
 
 export interface ConfirmDialogProps {
   open: boolean;
@@ -21,16 +18,6 @@ export interface ConfirmDialogProps {
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    paper: {
-      maxWidth: 800,
-      padding: theme.spacing(1, 3),
-      margin: theme.spacing(1),
-    },
     message: {
       margin: theme.spacing(3),
     },
@@ -50,53 +37,38 @@ export const ConfirmDialog: React.FC<ConfirmDialogProps> = props => {
   const classes = useStyles();
 
   return (
-    <Modal
-      open={open}
-      className={classes.root}
-      closeAfterTransition
-      BackdropComponent={Backdrop}
-      BackdropProps={{
-        timeout: 500,
-      }}
-      onClose={onClose}
-    >
-      <Fade in={open}>
-        <Paper className={classes.paper}>
-          <Typography
-            className={classes.message}
-            align="center"
-            variant="body1"
+    <ModalBase open={open} onClose={onClose}>
+      <>
+        <Typography className={classes.message} align="center" variant="body1">
+          {message}
+        </Typography>
+        <div className={classes.buttons}>
+          <Button
+            className={classes.button}
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              okClickHandler();
+              onClose();
+            }}
           >
-            {message}
-          </Typography>
-          <div className={classes.buttons}>
+            OK
+          </Button>
+          {cancelClickHandler && (
             <Button
               className={classes.button}
-              variant="contained"
-              color="primary"
+              variant="outlined"
+              color="default"
               onClick={() => {
-                okClickHandler();
+                cancelClickHandler();
                 onClose();
               }}
             >
-              OK
+              Cancel
             </Button>
-            {cancelClickHandler && (
-              <Button
-                className={classes.button}
-                variant="outlined"
-                color="default"
-                onClick={() => {
-                  cancelClickHandler();
-                  onClose();
-                }}
-              >
-                Cancel
-              </Button>
-            )}
-          </div>
-        </Paper>
-      </Fade>
-    </Modal>
+          )}
+        </div>
+      </>
+    </ModalBase>
   );
 };
