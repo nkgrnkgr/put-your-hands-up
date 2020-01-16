dea3345e151790ee11eb8f7ea3ceae6eb4ce27e5
nkgrnkgr <beefupme@gmail.com>
2020-01-16 22:27:44 +0900

    fix bug cannot edit event page

--- src/hooks/events.ts
+++ src/hooks/events.ts
@@ -3,12 +3,39 @@ import {
   getParticipatedEventList,
   getOrganizersEventList,
   getEventSnapshot,
+  getEvent,
 } from './../firebase/api/events';
 import { useEffect, useState, useMemo } from 'react';
 
 import { EventModel } from '../models/Event';
 
 export const useEvent = (eventId: string) => {
+  const [event, setEvent] = useState<EventModel | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const run = async () => {
+      try {
+        if (eventId !== '') {
+          const event = await getEvent(eventId);
+          setEvent(event);
+          setError(null);
+        }
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    run();
+  }, [eventId]);
+
+  return { event, loading, error };
+};
+
+export const useEventSnapshot = (eventId: string) => {
   const [event, setEvent] = useState<EventModel | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
